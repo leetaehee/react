15c478d0ca112790924ff7088332036d128a57fc
leetaehee <lastride25@naver.com>
2021-04-29 23:33:55 +0900

    커스텀 Hoos 만들기

--- chapter08/src/info.js
+++ chapter08/src/info.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react';
+import useInputs from './useInputs';
 
 function reducer(state, action) {
   return {
@@ -8,16 +9,12 @@ function reducer(state, action) {
 }
 
 const Info = () => {
-  const [state, dispatch] = useReducer(reducer, {
+  const [state, onChange] = useInputs({
     name: '',
     nickname: '',
   });
 
   const { name, nickname } = state;
-
-  const onChange = (e) => {
-    dispatch(e.target);
-  };
 
   return (
     <div>
