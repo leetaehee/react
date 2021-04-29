4287fd650f25244b57871f1b5269e2110863dcc5
leetaehee <lastride25@naver.com>
2021-04-29 22:52:30 +0900

    useReducer

--- chapter08/src/App.js
+++ chapter08/src/App.js
@@ -1,22 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Counter from './Counter';
-import Info from './info';
+import Info from './Info';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
   return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
+    <>
       <Counter />
-      {visible && <Info />}
-    </div>
+      <Info />
+    </>
   );
 };
 
