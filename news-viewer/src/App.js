5e654193df2000cc370d71241cbcbdb161c4fa1d
leetaehee <lastride25@naver.com>
2021-05-12 00:03:41 +0900

    14장 실습 마무리..

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,27 +1,9 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&category=entertainment&apiKey=bb1273db2cd74327a8c8896d83ed6603',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} />}
-    </div>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
