871e88126d38485a81085ebab715b4625888ee98
leetaehee <lastride25@naver.com>
2021-06-01 23:19:32 +0900

    redux-thunk 추가

--- learn-redux-middleware/src/modules/counter.js
+++ learn-redux-middleware/src/modules/counter.js
@@ -8,6 +8,19 @@ export const decrease = createAction(DECREASE);
 
 const initialState = 0; // 상태는 꼭 객체일 필요가 없습니다. 숫자도 작동해요.
 
+// 3초 뒤에 increase 혹은 decrease  함수를 디스패치함
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 3000);
+};
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 3000);
+};
+
 const counter = handleActions(
   {
     [INCREASE]: (state) => state + 1,
