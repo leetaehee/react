476f52ee6f6705bde67511cdb036d2ddc0b12d43
leetaehee <lastride25@naver.com>
2021-09-19 18:56:01 +0900

    blog 프로젝트 - 히원가입,로그인 프론트엔드 작업완료

--- blog/blog-frontend/src/components/auth/AuthForm.js
+++ blog/blog-frontend/src/components/auth/AuthForm.js
@@ -55,32 +55,53 @@ const ButtonWithMarginTop = styled(Button)`
     margin-top: 1rem;
 `;
 
+/**
+ * 에러를 보여 줍니다.
+ */
+const ErrorMessage = styled.div`
+    color: red;
+    text-align: center;
+    font-size: 0.875rem;
+    margin-top: 1rem;
+`;
+
 const textMap = {
     login: '로그인',
     register: '회원가입',
 }
 
-const AuthForm = ({ type }) => {
+const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
     const text = textMap[type];
     return (
         <AuthFormBlock>
             <h3>{text}</h3>
-            <form>
-                <StyledInput autoComplete="username" name="username" placeholder="아이디" />
+            <form onSubmit={onSubmit}>
+                <StyledInput 
+                    autoComplete="username" 
+                    name="username" 
+                    placeholder="아이디" 
+                    onChange={onChange}
+                    value={form.username}
+                />
                 <StyledInput
                     autoComplete="new-password"
                     name="password"
                     placeholder="비밀번호"
                     type="password"
+                    onChange={onChange}
+                    value={form.password}
                 />
                 {type === 'register' && (
                     <StyledInput 
                         autoComplete="new-password"
                         name="passwordConfirm"
-                        placeholder="비밀벊 학인"
+                        placeholder="비밀번호 학인"
                         type="password"
+                        onChange={onChange}
+                        value={form.passwordConfirm}
                     />
                 )}
+                {error && <ErrorMessage>{error}</ErrorMessage>}
                 <ButtonWithMarginTop cyan fullWidth style={{ marginTop: '1rem' }}>
                     {text}
                 </ButtonWithMarginTop>
